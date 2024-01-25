6c49f47ec0ebd920a7868685d37a17d3bbf828e3
pdu08075 <rpaeheh@naver.com>
2024-01-25 19:49:41 +0900

    dbms 'join' 1

--- practice/question.sql
+++ practice/question.sql
@@ -6,6 +6,13 @@
 -- team_id: 2, name: "LOTTE", city: "Busan", founded_year: 1982
 -- team_id: 3, name: "SK", city: "Incheon", founded_year: 2000
 
+insert into `teams`
+values(1, "DUSAN", "Seoul", 1999);
+insert into `teams`
+values(2, "LOTTE", "Busan", 1982);
+insert into `teams`
+values(3, "SK", "Incheon", 2000);
+
 # players 테이블에 다음과 같은 정보를 가진 5명의 선수를 추가
 -- player_id: 101, name: "Kim Min", position: "타자", birth_date: "1992-05-20"
 -- player_id: 102, name: "Lee Joon", position: "투수", birth_date: "1990-08-15"
@@ -13,16 +20,29 @@
 -- player_id: 104, name: "Park Seo", position: "외야수", birth_date: "1994-11-22"
 -- player_id: 105, name: "Jung Tae", position: "타자", birth_date: "1993-07-30"
 
+insert into `players`
+values(101, "Kim Min", "타자", "1992-05-20");
+insert into `players`
+values(102, "Lee Joon", "투수", "1990-08-15");
+insert into `players`
+values(103, "Choi Hyeon", "내야수", "1988-03-05");
+insert into `players`
+values(104, "Park Seo", "외야수", "1994-11-22");
+insert into `players`
+values(105, "Jung Tae", "타자", "1993-07-30");
 # 데이터 업데이트 (UPDATE)
 -- teams 테이블에서 team_id가 3인 팀의 이름을 "SSG"로 변경
 -- players 테이블에서 player_id가 105인 선수의 포지션을 "내야수"로 변경
 
+
 # 데이터 삭제 (DELETE)
 -- teams 테이블에서 founded_year가 2000년 이후인 모든 팀을 삭제하세요.
 -- players 테이블에서 1990년 1월 1일 이전에 태어난 선수들을 모두 삭제하세요.
+
 
 # 제약조건 추가
 -- teams 테이블에 team_id 컬럼에 대해 PRIMARY KEY 제약조건을 추가
 -- players 테이블에 player_id 컬럼에 대해 PRIMARY KEY 제약조건을 추가
 -- players 테이블의 birth_date 컬럼에 NOT NULL 제약조건을 추가
 -- teams 테이블의 name 컬럼에 UNIQUE 제약조건을 추가하여 모든 팀의 이름이 고유하도록 지정
+
